9e41086799faa0b9ab4fd5954f5ffe629af45d1d
Sahil Ninawe <sahilninawe13@gmail.com>
2025-02-12 00:33:25 +0530

    Adding Header Section (Good for Desktop)

--- app/page.js
+++ app/page.js
@@ -6,6 +6,17 @@ export default function Home() {
   const name = "Snail";
   return (
     <main>
+      {/* Header Section */}
+      <section className="bg-base-200">
+        <section className="flex justify-between items-center px-8 py-2 max-w-3xl mx-auto">
+          <div className="font-bold">CodeFastSaaS</div>
+          <div className="space-x-4">
+            <a className="link link-hover">Pricing</a>
+            <a className="link link-hover">FAQ</a>
+          </div>
+          <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+        </section>
+      </section>
       <section className="text-center py-32 px-8 max-w-3xl mx-auto">
         <h1 className="text-4xl font-extrabold mb-6">
           Collect customer feedback to build better products
