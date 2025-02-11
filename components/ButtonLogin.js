7858ab0634e529166fcc5e9f671f3e5dba8a673f
Sahil Ninawe <sahilninawe13@gmail.com>
2025-02-12 00:10:24 +0530

    Daisy UI Theme

--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -3,7 +3,11 @@ import Link from "next/link";
 // This Button is used to log users in or send them to the Login in page
 const ButtonLogin = ({ isLoggedIn, name }) => {
   if (isLoggedIn) {
-    return <Link href="/dashboard">Welcome back{name}</Link>;
+    return (
+      <Link href="/dashboard" className="btn btn-primary">
+        Welcome back {name} 👋
+      </Link>
+    );
   }
   return <button>Login</button>;
 };
