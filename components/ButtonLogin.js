63d96f4b415e54f6b0dc9b534c2b00680a352688
Sahil Ninawe <sahilninawe13@gmail.com>
2025-02-12 00:04:20 +0530

    Creating Hero Page

--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-const ButtonLogin = (props) => {
-  if (props.isLoggedIn) {
-    return <Link href="/dashboard">Welcome back{props.name}</Link>;
-  } else {
-    return <button>Login</button>;
+// This Button is used to log users in or send them to the Login in page
+const ButtonLogin = ({ isLoggedIn, name }) => {
+  if (isLoggedIn) {
+    return <Link href="/dashboard">Welcome back{name}</Link>;
   }
+  return <button>Login</button>;
 };
 
 export default ButtonLogin;
